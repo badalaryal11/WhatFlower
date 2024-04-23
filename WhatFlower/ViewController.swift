48e803233ee3e10bb9b4715c927820aa996a9e20
Badal Aryal <badal.aryal@gmail.com>
2024-04-23 18:02:21 +0545

    Get Classification from our model i.e FlowerClassification .mlmodel

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -22,10 +24,35 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let userPickedImage = info[UIImagePickerController.InfoKey.editedImage]
         
-        imageView.image = userPickedImage as? UIImage
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            guard let convertedCIImage = CIImage(image: userPickedImage) else {
+                fatalError("Cannot convert to CIImage.")
+            }
+            
+            detect(image: convertedCIImage  )
+            imageView.image = userPickedImage
+        }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Cannot import model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
     }
     @IBAction func CameraTapped(_ sender: Any) {
         present(imagePicker, animated: true, completion: nil)
